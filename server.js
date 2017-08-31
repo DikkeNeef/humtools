8b02628963ab70399e940aa5a98991b5c9fd2597
DikkeNeef <hans.gillis@ucb.com>
2017-08-31 07:23:35 +0200

    Working RPC example updated

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-var zerorpc    = require("zerorpc");
-var express    = require('express');
-var app        = express();
 var bodyParser = require('body-parser');
+var zerorpc = require("zerorpc");
+var express = require('express');
+var cors = require('cors');
+var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
+app.use(cors({
+    origin: '*'
+}));
 
 var port = process.env.PORT || 8000;
 var router = express.Router();
 var rpc = new zerorpc.Client();
 rpc.connect("tcp://127.0.0.1:4242");
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+router.get('/', function (req, res_) {
+    res_.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
 
-router.get('/hello/:name', function(req, res) {
-    rpc.invoke("hello", req.params.name, function(error, res, more) {
-        console.log(res);
+router.post('/hello', function (req, res_) {
+    rpc.invoke("hello", req.body.name, function (error, res, more) {
+        res_.json({
+            "name": res
+        });
     });
-    res.json({ message: 'Ahoi, Node.js server is responding' });
 })
-// http://localhost:8080/api
+
+// http://localhost:8000/api
 app.use('/api', router);
 app.listen(port);
