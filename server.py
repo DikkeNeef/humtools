decf71264fbb08aa81624cdc051a966998ae5110
DikkeNeef <hans.gillis@ucb.com>
2017-09-04 14:41:23 +0200

    server API + core mocker

--- server.py
+++ server.py
@@ -3,15 +3,17 @@
 File name:          server.py
 Author:             Dikke Neef
 Date created:       28/08/2017
-Date last modified: 01/09/2017
+Date last modified: 04/09/2017
 Python Version:     2.7.13
 """
 # ==============================================================================
+from ctypes import windll, Structure, c_long, byref
 from functools import partial
 from PIL import Image
 import threading
 import schedule
 import zerorpc
+import uuid
 import json
 import time
 
@@ -19,6 +21,27 @@ try:
     import cPickle as pickle
 except ImportError:
     import pickle
+
+
+class POINT(Structure):
+    _fields_ = [("x", c_long), ("y", c_long)]
+
+
+def get_mouse_position():
+    pt = POINT()
+    windll.user32.GetCursorPos(byref(pt))
+    return {"x": pt.x, "y": pt.y}
+
+
+def click(x, y):
+    windll.user32.SetCursorPos(x, y)
+    windll.user32.mouse_event(2, 0, 0, 0, 0)
+    windll.user32.mouse_event(4, 0, 0, 0, 0)
+
+
+def activate_window(x, y):
+    click(x, y)
+
 
 class Slicer(object):
     def __init__(self):
@@ -51,6 +74,11 @@ class Scheduler(object):
             time.sleep(1)
 
 
+def get_size(path_file):
+    im = Image.open(path_file)
+    return im.size
+
+
 def prettify(d):
     return json.dumps(d, indent=4, sort_keys=True)
 
@@ -61,6 +89,12 @@ def stringify(d):
 
 def load(path_file):
     return json.load(open(path_file, 'r'))
+
+
+def save(path_file, d):
+    with open(path_file, 'w') as f:
+        json.dump(d, f, indent=4, sort_keys=True)
+
 
 def loads(d):
     return json.loads(d)
@@ -120,6 +154,10 @@ class HumongousTools(object):
 
         return d
 
+    def load_conversation(self, scene, guid):
+        core = load(self.core)
+        return core[scene]["animations"][guid]["conv"]
+
     def load_object_properties(self, scene, guid):
         core = load(self.core)
         a = core[scene]["animations"][guid]
@@ -168,10 +206,6 @@ class HumongousTools(object):
     def backup(self):
         self.scheduler = Scheduler(partial(save_object, self))
 
-    def get_size(self, path_file):
-        im = Image.open(path_file)
-        return im.size
-
     def slice(self, post):
         blacklist = {}
         d = loads(post)
@@ -192,7 +226,7 @@ class HumongousTools(object):
                     for img in seq:
                         filename = img['i']
                         path_file = path + filename
-                        w, h = self.get_size(path_file)
+                        w, h = get_size(path_file)
                         if not path_file + ':%s,%s' % (img['x'], img['y']) in blacklist:
                             blacklist[path_file + ':%s,%s' % (img['x'], img['y'])] = {
                                 'x': img['x'],
@@ -209,6 +243,63 @@ class HumongousTools(object):
         else:
             slicer.slice(seconds, blacklist)
 
+    def save_object(self, a, scene, guid):
+        core = load(self.core)
+        save(self.folder + 'backup/' + uuid.uuid4() + '.json', core)
+
+        d = {
+            "name": a["name"],
+            "sound": a["sound"] if a["sound"] != "" else guid,
+            "script": a["script"],
+            "stop": a["stop"],
+            "params": a["params"],
+            "blacklist": a["blacklist"],
+            "singleton": a["singleton"],
+            "block": a["block"],
+            "loop": a["loop"],
+            "x": a["x"],
+            "y": a["y"],
+            "w": a["w"],
+            "h": a["h"],
+            "conv": {"dutch": [], "english": [], "french": [], "time": []},
+            "sequence": a["sequence"]
+        }
+
+        core[int(scene)]["animations"][guid] = d
+        save(self.core, core)
+        return self.load_object_tree()
+
+    def save_conversations(self, conv, scene, guid):
+        core = load(self.core)
+        save(self.folder + 'backup/' + uuid.uuid4() + '.json', core)
+        core[int(scene)]["animations"][guid]["conv"] = conv
+        save(self.core, core)
+        return self.load_conversation(scene, guid)
+
+    def add_scene(self):
+        core = load(self.core)
+        save(self.folder + 'backup/' + uuid.uuid4() + '.json', core)
+
+        d = {
+            "container": {},
+            "mapper": {}
+        }
+
+        core.append(d)
+        save(self.core, core)
+        return self.load_scene_tree()
+
+    def record_audio(self, x, y, scene):
+        path_file = [
+            "%s%s/%s/music-%s.mp3" % (self.folder, "tmp", str(scene), uuid.uuid4()),
+            "%s%s/%s/music-%s.mp3" % (self.folder, "tmp", str(scene), uuid.uuid4())
+        ]
+        # TODO record device audio - click on window, start recording
+        # TODO save it to path_file for editing purposes
+
+    def capture_background(self, x, y, scene, w=640, h=480):
+        path_file = "%s%s/%s/background+%s.png" % (self.folder, "tmp", str(scene), uuid.uuid4())
+        # TODO screenshot - (x, y, x+w, y+h) -> save to path_file
 
 
 def main():
