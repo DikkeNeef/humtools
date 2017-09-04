3b9432b2220adb5a567c785a62f8c265be108c39
DikkeNeef <hans.gillis@ucb.com>
2017-09-04 12:06:02 +0200

    server API + slice mocker

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ Python Version:     2.7.13
 """
 # ==============================================================================
 from functools import partial
+from PIL import Image
 import threading
 import schedule
 import zerorpc
@@ -18,6 +19,21 @@ try:
     import cPickle as pickle
 except ImportError:
     import pickle
+
+class Slicer(object):
+    def __init__(self):
+        pass
+
+    def slice(self, seconds, blacklist, d=None):
+        if d is not None:
+            x = d['x']
+            y = d['y']
+            w = d['w']
+            h = d['h']
+            # TODO - slice specific area
+        else:
+            pass
+            # TODO - slice full area
 
 
 class Scheduler(object):
@@ -35,8 +51,19 @@ class Scheduler(object):
             time.sleep(1)
 
 
+def prettify(d):
+    return json.dumps(d, indent=4, sort_keys=True)
+
+
 def stringify(d):
     return json.dumps(d)
+
+
+def load(path_file):
+    return json.load(open(path_file, 'r'))
+
+def loads(d):
+    return json.loads(d)
 
 
 def save_object(obj, fn='zerorpc'):
@@ -50,20 +77,145 @@ def open_object(fn='zerorpc'):
 
 
 class HumongousTools(object):
-    def __init__(self):
+    def __init__(self, folder="work/"):
+        self.core = folder + "core.json"
         self.scheduler = None
+        self.folder = folder
+
+    def total_scenes(self):
+        core = load(self.core)
+        scenes = len(core)
+        return scenes
+
+    def get_animations(self, scene):
+        core = load(self.core)
+        _scene = core[scene]
+        for animation in _scene["animations"]:
+            yield {"guid": animation, "name": animation["name"]}
+
+    def get_conversations(self, scene):
+        core = load(self.core)
+        _scene = core[scene]
+        for animation in _scene["animations"]:
+            yield {"guid": animation, "name": animation["conv"]}
+
+    def load_object_tree(self):
+        d = {}
+        for scene in range(self.total_scenes()):
+            d[str(scene)] = []
+            for animation in self.get_animations(scene):
+                d[str(scene)].append(animation)
+
+        return d
+
+    def load_scene_tree(self):
+        return {"scenes": self.total_scenes()}
+
+    def load_conversation_tree(self):
+        d = {}
+        for scene in range(self.total_scenes()):
+            d[str(scene)] = []
+            for animation in self.get_conversations(scene):
+                d[str(scene)].append(animation)
+
+        return d
+
+    def load_object_properties(self, scene, guid):
+        core = load(self.core)
+        a = core[scene]["animations"][guid]
+        return {
+            "guid": guid,
+            "name": a["name"],
+            "sound": a["sound"] if a["sound"] != "" else guid,
+            "script": a["script"],
+            "stop": a["stop"],
+            "params": a["params"],
+            "blacklist": a["blacklist"],
+            "singleton": a["singleton"],
+            "block": a["block"],
+            "loop": a["loop"],
+            "x": a["x"],
+            "y": a["y"],
+            "w": a["w"],
+            "h": a["h"]
+        }
+
+    def load_sound(self, scene, guid):
+        core = load(self.core)
+        a = core[scene]["animations"][guid]
+        return {
+            "path_file": [
+                '%s%s/%s/animations/%s/%s.mp3' % (
+                    self.folder, "scene", str(scene), guid, a["sound"] if a["sound"] != "" else guid
+                ),
+                '%s%s/%s/animations/%s/%s.ogg' % (
+                    self.folder, "scene", str(scene), guid, a["sound"] if a["sound"] != "" else guid
+                )
+            ]
+        }
+
+    def load_background(self, scene):
+        return {"path_file": '%s%s/%s/background.png' % (self.folder, "scene", str(scene))}
+
+    def load_music(self, scene):
+        return {
+            "path_file": [
+                '%s%s/%s/music.mp3' % (self.folder, "scene", str(scene)),
+                '%s%s/%s/music.ogg' % (self.folder, "scene", str(scene))
+            ]
+        }
 
     def backup(self):
         self.scheduler = Scheduler(partial(save_object, self))
 
-    def hello(self, name):
-        return "Hello, " + name
+    def get_size(self, path_file):
+        im = Image.open(path_file)
+        return im.size
+
+    def slice(self, post):
+        blacklist = {}
+        d = loads(post)
+        x = d["x"]
+        y = d["y"]
+        w = d["w"]
+        h = d["h"]
+        scene = d["scene"]
+        seconds = d["seconds"]
+
+        slicer = Slicer()
+        for animation in self.get_animations(scene):
+            guid = animation["guid"]
+            properties = self.load_object_properties(scene, guid)
+            if properties["blacklist"]:
+                path = self.folder + "tmp/" + str(scene) + '/' + str(guid) + '/slice/'
+                for seq in properties['sequence']:
+                    for img in seq:
+                        filename = img['i']
+                        path_file = path + filename
+                        w, h = self.get_size(path_file)
+                        if not path_file + ':%s,%s' % (img['x'], img['y']) in blacklist:
+                            blacklist[path_file + ':%s,%s' % (img['x'], img['y'])] = {
+                                'x': img['x'],
+                                'y': img['y'],
+                                'w': w,
+                                'h': h,
+                                'i': path_file
+                            }
+
+        print(prettify(blacklist))
+
+        if d["blacklist"]:
+            slicer.slice(seconds, blacklist, {'x': x, 'y': y, 'w': w, 'h': h})
+        else:
+            slicer.slice(seconds, blacklist)
+
 
 
 def main():
     reload_server = True
     server = HumongousTools()
-    if not reload_server: server = open_object()
+    if not reload_server:
+        server = open_object()
 
     server.backup()
     s = zerorpc.Server(server)
